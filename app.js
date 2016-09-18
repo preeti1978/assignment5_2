c21e8a74cb27aac9ad82434a47e8a46637b0f4c4
preeti1978 <lekshmy.money@gmail.com>
2016-09-18 13:38:21 +0530

    printing using java script object literal

---
+++ app.js
@@ -0,0 +1,12 @@
+//creating javascript object using object literal notation
+let myObject = {
+    name: 'preeti',
+    age: 38,
+    dateofbirth: '12-07-1978',
+    placeofbirth:"Trivandrum"
+};
+
+console.log("name: "+myObject.name);
+console.log("age: "+myObject.age);
+console.log("DOB: "+myObject.dateofbirth);
+console.log("place of birth: "+myObject.placeofbirth);
